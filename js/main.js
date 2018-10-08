b4a7bf7c22fd5c661d2cd1d64a23e421a58ef661
JewelurRahman <jewelurrahman@ymail.com>
2018-10-08 22:37:32 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+wow = new WOW(); 
+      wow.init();
+// Sliders Init
+
+
+$('.owl-schedule').owlCarousel({
+    rtl:false,
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})  
+
+		
+$('.owl-twitter').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
